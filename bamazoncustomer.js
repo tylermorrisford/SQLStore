397524eb8a70f233994b9ed50ceff5158c845a2f
tylermorrisford <tylerford@gmail.com>
2019-09-22 13:42:27 -0600

    create transaction function, needs refactor

--- bamazoncustomer.js
+++ bamazoncustomer.js
@@ -16,13 +16,41 @@ function displayItems() {
         "SELECT * FROM products",
         function(err, res) {
             if (err) throw err;
+            console.log("\n");
             for (var i=0; i<res.length; i++) {
               console.log(res[i].item_id + " | " + res[i].item + " | " + res[i].price);      
             }
-            // console.log(res);
-        }
-    );
+            console.log("\n");
+        },
+        transaction()
+    )
 }
+
+function transaction() {
+    inquire.prompt([
+        {
+            type: 'input',
+            name: 'productChoice',
+            message: "Please enter the id # of the item you would like to purchase:",
+        //     choices: ["t-shirt", 
+        //     "jeans / id 1", 
+        //     "jacket", 
+        //     "running shoes", 
+        //     "hiking shoes", 
+        //     "boots", 
+        //     "toothbrush", 
+        //     "deodorant",
+        //     "shampoo",
+        //     "conditioner" 
+        // ]
+    },
+      ])
+      .then(answer => {
+        console.log("Great choice, the " + res[(answer.productChoice - 1)]);
+      })
+}
+
 // for inventory check, create variable that stores item_id -1 as index of res
 // then compare res[i].stock (may have to convert to number) to user request
 displayItems();
+transaction();
