73f9505f350f9634c00984ed59ede20a2017c94c
tylermorrisford <tylerford@gmail.com>
2019-09-23 13:20:20 -0600

    work on async errors

--- bamazoncustomer.js
+++ bamazoncustomer.js
@@ -1,6 +1,7 @@
 require('dotenv').config();
 var mysql = require("mysql");
 var inquire = require("inquirer");
+var res = " ";
 // 
 var connection = mysql.createConnection({
     host: "localhost",
@@ -19,14 +20,9 @@ function displayItems() {
             console.log("\n");
             for (var i=0; i<res.length; i++) {
               console.log(res[i].item_id + " | " + res[i].item + " | " + res[i].price);      
-            }
+            };
             console.log("\n");
         },
-        transaction()
-    )
-}
-
-function transaction() {
     inquire.prompt([
         {
             type: 'input',
@@ -46,11 +42,33 @@ function transaction() {
     },
       ])
       .then(answer => {
-        console.log("Great choice, the " + res[(answer.productChoice - 1)]);
-      })
-}
+        var userSelection = answer.productChoice;
+        connection.query(
+            "SELECT item FROM products WHERE ?",
+            { item_id: userSelection },
+            function(err, res) {
+                if (err) throw err;
+                // console.log("Great choice, the " + answer.productChoice);
+                console.log("Great choice, the " + res[0].item) = "!";
+                
+            },
+
+      )}),
+          inquire.prompt([
+              {
+              type: 'input',
+              message: "How many would you like?",
+              answer: "quantity"
+          }
+          ]).then(answer => {
+              console.log("Hold on a moment while I check our stock");
+              console.log(res[0].item);
+              console.log(answer.quantity);
+          })
+      
+      )}
 
 // for inventory check, create variable that stores item_id -1 as index of res
 // then compare res[i].stock (may have to convert to number) to user request
 displayItems();
-transaction();
+// transaction();
