9208459493a8475c40f187a56f8333df9d9578e5
tylermorrisford <tylerford@gmail.com>
2019-09-24 22:46:29 -0600

    encounter mysql error

--- bamazoncustomer.js
+++ bamazoncustomer.js
@@ -10,9 +10,14 @@ var connection = mysql.createConnection({
     database: "bamazon"
 });
 
+connection.on('error', function(err) {
+    console.log("[mysql error]",err);
+  });
+
 function displayItems() {
     console.log('\x1b[33m%s\x1b[0m' ,'Welcome to Bamazon, home of the best deals in the known universe!');
-    var query = connection.query(
+    // var query = 
+    connection.query(
         "SELECT * FROM products",
         function(err, res) {
             if (err) throw err;
@@ -38,37 +43,64 @@ function displayItems() {
                 var reqAmount = answer.quantity;
                 var inStock = res[userSelection].stock - reqAmount;
                 console.log('\x1b[36m%s\x1b[0m', "The " + res[userSelection].item + ", great choice! \nOne moment while I check to see if we have " + reqAmount + " available...");
-                connection.query(
-                    "SELECT * FROM products WHERE ?",
-                    { item_id: userSelection },
+                if (res[0].stock >= reqAmount) {
+                    console.log('\x1b[32m%s\x1b[0m', "Good news! Your order is being processed.");
+                    var query = connection.query(
+                        "UPDATE products SET ? WHERE ?",
+                    {stock: inStock},
+                    {item_id: userSelection},
                     function(err, res) {
-                        if (err) throw err;
-                        console.log('we have ' + inStock + ' in stock');
-                        // console.log("Great choice, the " + answer.productChoice);
-                        if (res[0].stock >= reqAmount) {
-                            console.log('your order is being processed');
-                            // processOrder(userSelection, inStock); 
-                        } else {
-                            console.log('\x1b[35m%s\x1b[0m', "So sorry; we're not able to process your order.")
-                        }
-                    },
-              )})
-        },
-      )}
+                        console.log('query starting');
+                    if (err) throw err;
+                    console.log("\n");
+                    for (var i=0; i<res.length; i++) {
+                      console.log(res[i].item_id + " | " + res[i].item + " | " + res[i].price);      
+                    };
+                }
+                    )} else {
+                        console.log('\x1b[35m%s\x1b[0m', "So sorry; we're not able to process your order.")
+                    }
+                })
+            },
+          )}
+                
+                
+                
+                
+                
+                
+                
+//                 connection.query(
+//                     "SELECT * FROM products WHERE ?",
+//                     { item_id: userSelection },
+//                     function(err, res) {
+//                         if (err) throw err;
+//                         console.log('we have ' + inStock + ' in stock');
+//                         // console.log("Great choice, the " + answer.productChoice);
+//                         if (res[0].stock >= reqAmount) {
+//                             console.log('your order is being processed');
+//                             // processOrder(userSelection, inStock); 
+//                         } else {
+//                             console.log('\x1b[35m%s\x1b[0m', "So sorry; we're not able to process your order.")
+//                         }
+//                     },
+//               )})
+//         },
+//       )}
 
-function processOrder(selection, amount) {
-    console.log('\x1b[32m%s\x1b[0m', "Good news! Your order is being processed.")
-    var query = connection.query(
-        "UPDATE products SET ? WHERE ?",
-        {stock: amount},
-        {item_id: selection},
-        function(err, res) {
-        if (err) throw err;
-        console.log("\n");
-        for (var i=0; i<res.length; i++) {
-          console.log(res[i].item_id + " | " + res[i].item + " | " + res[i].price);      
-        };
-})
-}
+// function processOrder(selection, amount) {
+//     console.log('\x1b[32m%s\x1b[0m', "Good news! Your order is being processed.")
+//     var query = connection.query(
+//         "UPDATE products SET ? WHERE ?",
+//         {stock: amount},
+//         {item_id: selection},
+//         function(err, res) {
+//         if (err) throw err;
+//         console.log("\n");
+//         for (var i=0; i<res.length; i++) {
+//           console.log(res[i].item_id + " | " + res[i].item + " | " + res[i].price);      
+//         };
+// })
+// }
 
 displayItems();
