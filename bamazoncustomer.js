4b15e20c1f7dd77c32b5f7e010514b848618904c
tylermorrisford <tylerford@gmail.com>
2019-09-24 22:08:24 -0600

    began refactor for callback query update

--- bamazoncustomer.js
+++ bamazoncustomer.js
@@ -16,9 +16,9 @@ function displayItems() {
         "SELECT * FROM products",
         function(err, res) {
             if (err) throw err;
-            console.log("\n");
+            console.log("\n  ID    PRODUCT       PRICE    STOCK");
             for (var i=0; i<res.length; i++) {
-              console.log(res[i].item_id + " | " + res[i].item + " | " + res[i].price);      
+              console.log("  " + res[i].item_id + "   | " + res[i].item + "       | " + res[i].price + " | " + res[i].stock);      
             };
             console.log("\n");
             inquire.prompt([
@@ -34,19 +34,20 @@ function displayItems() {
             }
               ])
               .then(answer => {
-                var userSelection = answer.productChoice;
+                var userSelection = (answer.productChoice - 1);
                 var reqAmount = answer.quantity;
+                var inStock = res[userSelection].stock - reqAmount;
+                console.log('\x1b[36m%s\x1b[0m', "The " + res[userSelection].item + ", great choice! \nOne moment while I check to see if we have " + reqAmount + " available...");
                 connection.query(
                     "SELECT * FROM products WHERE ?",
                     { item_id: userSelection },
                     function(err, res) {
                         if (err) throw err;
-                        var inStock = res[0].stock - reqAmount;
                         console.log('we have ' + inStock + ' in stock');
                         // console.log("Great choice, the " + answer.productChoice);
-                        console.log('\x1b[36m%s\x1b[0m', "The " + res[0].item + ", great choice! \nOne moment while I check to see if we have " + reqAmount + " available...");
                         if (res[0].stock >= reqAmount) {
-                            processOrder(userSelection, inStock); 
+                            console.log('your order is being processed');
+                            // processOrder(userSelection, inStock); 
                         } else {
                             console.log('\x1b[35m%s\x1b[0m', "So sorry; we're not able to process your order.")
                         }
@@ -57,17 +58,17 @@ function displayItems() {
 
 function processOrder(selection, amount) {
     console.log('\x1b[32m%s\x1b[0m', "Good news! Your order is being processed.")
-    connection.query(
+    var query = connection.query(
         "UPDATE products SET ? WHERE ?",
         {stock: amount},
-        {item_id: selection}
-    ), function(err, res) {
+        {item_id: selection},
+        function(err, res) {
         if (err) throw err;
         console.log("\n");
         for (var i=0; i<res.length; i++) {
           console.log(res[i].item_id + " | " + res[i].item + " | " + res[i].price);      
         };
-}
+})
 }
 
 displayItems();
